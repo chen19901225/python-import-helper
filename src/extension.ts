e4f9c3f51ebb8710181045a8e7e87c586b23bb9a
chenqinghe <1832866299@qq.com>
2019-05-24 22:38:22 +0800

    still call python.sortImports

--- src/extension.ts
+++ src/extension.ts
@@ -100,14 +100,7 @@ export function activate(context: vscode.ExtensionContext) {
             }
         }
 
-        // find non empty
-        // let j = i - 1;
-        // while (1 && j >= 0) {
-        //     line = document.lineAt(j).text;
-        //     if (!Object.is("", line.trim())) {
-        //         break;
-        //     }
-        // }
+       
 
 
         let textAppendToEmpty = async (text: string, i: number) => {
@@ -128,52 +121,11 @@ export function activate(context: vscode.ExtensionContext) {
 
         const selection = textEditor.selection;
         const text = textEditor.document.getText(selection);
-        if (text.startsWith("import")) {
-            await textAppendToEmpty(text, i);
-        } else {
-            // find insert line or insert the last
-            let the_same_from_row_index = -1;
-            let walk_row_index = i;
-            let words = text.split(" ");
-            let compare_line;
-            while (1) {
-                compare_line = document.lineAt(walk_row_index);
-                if(!compare_line.text.startsWith("from") && !compare_line.text.startsWith("import")) {
-                    let [is_ok, new_i] = handle_line(walk_row_index);
-                    if(is_ok) {
-                        break;
-                    } else {
-                        walk_row_index = new_i;
-                        continue;
-                    }
-                    // break;
-                }
-                let line_words: Array<string> = compare_line.text.split(" ");
-                if (Object.is(line_words[1], words[1])) {
-                    the_same_from_row_index = walk_row_index;
-                    edit.replace(selection, "");
-                    
-                    if(line_words.indexOf(words[3]) > -1) {
-
-                    } else {
-                        edit.replace(compare_line.range, compare_line.text + ", " + words[3]);
-                    }
-                    await vscode.commands.executeCommand("editor.action.deleteLines");
-                    break;
-                } else {
-                    walk_row_index += 1;
-                }
-                
-            }
-
-            if(the_same_from_row_index==-1) {
-                await textAppendToEmpty(text, i);
-            } 
-        }
-
+        await textAppendToEmpty(text, i);
+        
         // excute python sort import
-        // await vscode.commands.executeCommand("python.sortImports"); 
-        // python.sortImports seem unreliable
+        await vscode.commands.executeCommand("python.sortImports"); 
+        
 
 
 
