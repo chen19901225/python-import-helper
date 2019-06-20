4364603c56030b99c499fed5d96b78f2537d9434
chenqinghe <1832866299@qq.com>
2019-06-21 07:45:41 +0800

    test

--- src/function_apply_self.ts
+++ src/function_apply_self.ts
@@ -1,16 +1,38 @@
 import * as vscode from "vscode";
 export function function_apply_self(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
     let currentPosition = textEditor.selection.active;
+    // 当前行的缩进
     let currentLineIndent = textEditor.document.lineAt(currentPosition.line).firstNonWhitespaceCharacterIndex;
+    let defLineNo = -1;
+    for (let i = currentPosition.line; i >= 0; i--) {
+        let warkLine = textEditor.document.lineAt(i);
+        if (warkLine.text.startsWith("def ") &&
+            warkLine.firstNonWhitespaceCharacterIndex + 4 === currentLineIndent) {
+            defLineNo = i;
+            break;
+        }
+    }
+    if (defLineNo === -1) {
+        throw Error("cannot find def");
+    }
+    // 找到defLineNo之后， 开始找def的结束
+    let defEndLineNo=-1;
+    for(let i=defLineNo;i<=currentPosition.line;i++) {
+
+    }
+    if(defEndLineNo === -1) {
+        throw Error("cannot find def end line");
+    }
     
 
-    const definition=
+
+    const definition =
 }
 
 
 function getIndent(text: string) {
     const match = text.match(/^\s+/);
-    if(!match) {
+    if (!match) {
         return 0;
     }
     return match[0].length;
