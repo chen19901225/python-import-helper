fdb7c4589b67b0263608d6520520ffbfe2bf1b0b
chenqinghe <1832866299@qq.com>
2019-06-20 23:49:16 +0800

    test

--- src/function_apply_self.ts
+++ src/function_apply_self.ts
@@ -1,4 +1,17 @@
 import * as vscode from "vscode";
 export function function_apply_self(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
+    let currentPosition = textEditor.selection.active;
+    let currentLineIndent = textEditor.document.lineAt(currentPosition.line).firstNonWhitespaceCharacterIndex;
     
+
+    const definition=
+}
+
+
+function getIndent(text: string) {
+    const match = text.match(/^\s+/);
+    if(!match) {
+        return 0;
+    }
+    return match[0].length;
 }
