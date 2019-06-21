921e43b1731a84430531124d5c7daa8f269b358d
chen19901225 <1832866299@qq.com>
2019-06-21 09:51:14 +0800

    test

--- src/parser.ts
+++ src/parser.ts
@@ -1,15 +1,14 @@
 
 export function parse_function(definition: string) {
-    const lines: Array<string> = definition.split("\n");
 
 }
 
 function parse_function_name(definition: string) {
-    let match = definition.match('def\s*(\w+)\s*');
+    let match = definition.match(/def \s*(\w+)\s*\(/);
     if(!match){
         return "";
     }
-    return match[0];
+    return match[1];
 }
 
 function parse_args(definition: string) {
@@ -18,3 +17,11 @@ function parse_args(definition: string) {
 function parse_kwargs(definition: string) {
 
 }
+
+let definition=`def hello_world_func(no_type, name: str,
+    kw_no_type,
+    kw_no_type_and_default=10
+    name2:str = "hello") -> List[str, str]:`;
+
+let function_name = parse_function_name(definition);
+console.log("function_name", function_name);
