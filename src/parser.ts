d629d0828b976d0bcea5455ef7c1040b77a869a1
chen19901225 <1832866299@qq.com>
2019-06-21 09:34:56 +0800

    test

--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,20 @@
 
 export function parse_function(definition: string) {
-    
+    const lines: Array<string> = definition.split("\n");
+
+}
+
+function parse_function_name(definition: string) {
+    let match = definition.match('def\s*(\w+)\s*');
+    if(!match){
+        return "";
+    }
+    return match[0];
+}
+
+function parse_args(definition: string) {
+
+}
+function parse_kwargs(definition: string) {
+
 }
