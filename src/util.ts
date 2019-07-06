74a79d6751413b59f9c522a9ca6cca159fd2373a
chenqinghe <1832866299@qq.com>
2019-07-06 09:17:56 +0800

    test

--- src/util.ts
+++ src/util.ts
@@ -21,6 +21,9 @@ export function try_get_definition(textEditor: vscode.TextEditor, edit: vscode.T
     let defLineNo = -1;
     for (let i = currentPosition.line; i >= 0; i--) {
         let warkLine = textEditor.document.lineAt(i);
+        if(warkLine.isEmptyOrWhitespace) {
+            continue;
+        }
         let warkLineIndex = warkLine.firstNonWhitespaceCharacterIndex;
         
         if(warkLine.firstNonWhitespaceCharacterIndex>=currentMinIndent) {
