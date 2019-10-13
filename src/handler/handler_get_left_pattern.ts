bfcb4feea5494c033bb82f37d3395d6ffb831a14
chenqinghe <1832866299@qq.com>
2019-10-13 18:35:12 +0800

    test

--- src/handler/handler_get_left_pattern.ts
+++ src/handler/handler_get_left_pattern.ts
@@ -1,6 +1,11 @@
 import * as vscode from "vscode";
 import { error } from "util";
-import {removeVarType} from "../util"
+import { removeVarType } from "../util"
+
+function _format(text: string): string {
+    let origin = removeVarType(text)
+    return origin.trim()
+}
 
 
 export function get_insert_context(text: string): [boolean, string] {
@@ -8,14 +13,14 @@ export function get_insert_context(text: string): [boolean, string] {
     if (match) {
         let pattern = match[1];
         // edit.insert(position, pattern.trim());
-        return [true, removeVarType(pattern)];
+        return [true, _format(pattern)];
         // return;
     }
     match = /^\s*'([^']+)'\:.*$/.exec(text);
     if (match) {
         let pattern = match[1];
         // edit.insert(position, pattern.trim());
-        return [true, removeVarType(pattern)];
+        return [true, _format(pattern)];
         // return;
     }
 
@@ -23,7 +28,7 @@ export function get_insert_context(text: string): [boolean, string] {
     if (match) {
         let pattern = match[1];
         // edit.insert(position, pattern.trim());
-        return [true, removeVarType(pattern)];
+        return [true, _format(pattern)];
         // return;
     }
     return [false, ""]
@@ -35,7 +40,7 @@ export function insert_left_pattern(textEditor: vscode.TextEditor, edit: vscode.
     let currentLine = textEditor.document.lineAt(position.line);
     let text = currentLine.text.trim();
     let [found, inserted_text] = get_insert_context(text);
-    if(found) {
+    if (found) {
         edit.insert(position, inserted_text);
     }
     // if(/^[^=]+=.+$/.test())
