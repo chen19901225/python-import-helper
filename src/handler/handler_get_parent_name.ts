82cafc0770e835516dfde491378b7f9a7c3b6523
chenqinghe <1832866299@qq.com>
2019-11-01 08:05:43 +0800

    test

--- src/handler/handler_get_parent_name.ts
+++ src/handler/handler_get_parent_name.ts
@@ -5,8 +5,37 @@ import { try_get_definition } from '../util'
 export function get_parent_name(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
     const definition = try_get_definition(textEditor, edit);
     const parseResult = parse_function(definition);
-    
-    const insertContent = parseResult.name;
+
+    let insertContent = parseResult.name;
     let currentPosition = textEditor.selection.active;
-    edit.insert(currentPosition, insertContent);
+    let quickItems: vscode.QuickPickItem[] = []
+    for (let name of ["raw", "raw_without_prefix"]) {
+        quickItems.push({
+            "label": name,
+            "description": name
+        })
+    }
+    vscode.window.showQuickPick(quickItems).then((item) => {
+        if (item) {
+            let { label } = item;
+            if (label === "raw") {
+                // edit.insert(currentPosition, insertContent);
+            } else {
+                if (insertContent.startsWith("_")) {
+                    insertContent = insertContent.slice(1)
+                }
+                let index = insertContent.indexOf("_")
+                insertContent = insertContent.slice(index + 1);
+
+            }
+            let convert_element = insertContent;
+            let activeEditor = vscode.window.activeTextEditor;
+            activeEditor.insertSnippet(new vscode.SnippetString(convert_element), textEditor.selection.active)
+
+            // edit.insert(currentPosition, insertContent);
+        }
+    })
+
+    
+
 }
