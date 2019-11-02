37d574a468b7b212b79d0794f7c023b4e4d58afd
chenqinghe <1832866299@qq.com>
2019-11-02 12:51:11 +0800

    test

--- src/handler/handler_get_last_used_variable.ts
+++ src/handler/handler_get_last_used_variable.ts
@@ -1,14 +1,40 @@
 import * as vscode from "vscode";
 let _last_used_variable: string = null;
+let used_variables: Array<string> = [];
 export function get_last_used_variable(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
-    if (_last_used_variable === null) {
+    if (used_variables.length === 0) {
         vscode.window.showErrorMessage("last_used_variable is null");
         return;
     }
-    let position = textEditor.selection.active;
-    edit.insert(position, _last_used_variable);
+    if(used_variables.length === 1) {
+        edit.insert(position, used_variables[0]);
+    }
+    let quickItems: vscode.QuickPickItem[] = []
+    for(let i=used_variables.length -1; i>=0 ;i--) {
+        let current_var = used_variables[i];
+        quickItems.push({
+            'label': current_var,
+            'description': current_var
+        })
+    }
+    let currentPosition = textEditor.selection.active;
+    vscode.window.showQuickPick(quickItems).then((item) => {
+        if(item) {
+            let current_var = item.label;
+            let activeEditor = vscode.window.activeTextEditor;
+            
+            activeEditor.insertSnippet(new vscode.SnippetString(current_var), currentPosition)
+        }
+    })
+    
+    
 
 }
 export function update_last_used_variable(current_var: string) {
-    _last_used_variable = current_var
+    let index = used_variables.indexOf(current_var);
+    if(index > -1) {
+        used_variables.splice(index, 1);
+    }
+    used_variables.push(current_var);
+    // _last_used_variable = current_var
 }
