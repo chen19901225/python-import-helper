37f7daf7c407e7d950d6dac1343fd4c6707a3368
chenqinghe <1832866299@qq.com>
2019-11-03 09:16:35 +0800

    add quick pick item

--- src/handler/handler_get_left_last_part.ts
+++ src/handler/handler_get_left_last_part.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
-import {removeVarType} from "../util"
-export function get_left_last_part(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) { 
+import { removeVarType } from "../util"
+export function get_left_last_part(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
     let position = textEditor.selection.active;
     let currentLine = textEditor.document.lineAt(position.line);
     let text = currentLine.text.trim();
@@ -9,13 +9,41 @@ export function get_left_last_part(textEditor: vscode.TextEditor, edit: vscode.T
     if (match) {
         let eleWithoutType = removeVarType(match[1])
         let pattern = eleWithoutType
-        if(eleWithoutType.indexOf(".") > -1) {
+        if (eleWithoutType.indexOf(".") > -1) {
             pattern = eleWithoutType.split(".").pop();
-        } else if(eleWithoutType.indexOf("__") > -1) {
+        } else if (eleWithoutType.indexOf("__") > -1) {
             let index = eleWithoutType.indexOf("__")
-            pattern = eleWithoutType.slice(index+2)
+            pattern = eleWithoutType.slice(index + 2)
         }
-        edit.insert(position, pattern.trim());
+        let quickItems: vscode.QuickPickItem[] = [
+            {
+                'label': '1.raw',
+                'description': '1.keep private, self._no_keep_alive got _no_keep_alive'
+            },
+            {
+                "label": "2.remove_private",
+                "description": "2. remove private prefix, self._no_keep_alive, get no_keep_alive"
+            }
+        ]
+        vscode.window.showQuickPick(quickItems).then((item) => {
+            if (item) {
+                let { label } = item;
+                if (label.startsWith("1.")) {
+                    
+                } else {
+                    if(pattern.startsWith("_")) {
+                        pattern = pattern.slice(1)
+                    }
+                }
+
+                let activeEditor = vscode.window.activeTextEditor;
+                // let insertedText = getInsertedContent(item.description);
+                activeEditor.insertSnippet(new vscode.SnippetString(pattern), position);
+            }
+        })
+
+
+        // edit.insert(position, pattern.trim());
         return;
     }
 
