591df6d2b1df03ce4ee0cd9dfe685065a4ad576d
chenqinghe <1832866299@qq.com>
2019-12-13 23:03:37 +0800

    fix insert_last_import

--- src/handler/handler_insert_last_import.ts
+++ src/handler/handler_insert_last_import.ts
@@ -3,11 +3,16 @@ import { error } from "util";
 
 export function insert_last_import(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
     let cursor = textEditor.selection.active;
-    let lastLine = textEditor.document.lineAt(cursor.line - 1);
-    let [flag, import_str] = get_last_import(lastLine.text);
-    if (flag) {
-        edit.insert(cursor, import_str);
+    for (let i = 0; i < 20; i++) {
+        let currentLineNum = cursor.line - i;
+        let lastLine = textEditor.document.lineAt(currentLineNum);
+        let [flag, import_str] = get_last_import(lastLine.text);
+        if (flag) {
+            edit.insert(cursor, import_str);
+            break;
+        }
     }
+
 }
 
 export function get_last_import(text: string): [boolean, string] {
