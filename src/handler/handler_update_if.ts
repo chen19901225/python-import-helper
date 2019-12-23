7947cbfe3b5036771cfcea5eaf5237af25080af1
chen19901225 <1832866299@qq.com>
2019-12-23 11:10:02 +0800

    'ci'

--- src/handler/handler_update_if.ts
+++ src/handler/handler_update_if.ts
@@ -6,9 +6,17 @@ export function update_if(textEditor: vscode.TextEditor, edit: vscode.TextEditor
     let lastLineNo = position.line - 1;
     let lastLineText = textEditor.document.lineAt(lastLineNo).text;
     lastLineText = lastLineText.trim();
-    if(lastLineText.startsWith("if ") || lastLineText.startsWith("elif ")) {
+    if (lastLineText.startsWith("if ") || lastLineText.startsWith("elif ")) {
         let last_var = lastLineText.split(/\s+/)[1];
         edit.insert(position, `[${last_var}] = ${last_var}`);
     }
     // edit.insert(position, "self");
+}
+
+export function update_if_get_line_var(text: string): string {
+    let last_var = text.split(/\s+/)[1];
+    if (last_var.endsWith(":")) {
+        last_var = last_var.slice(0, last_var.length - 1);
+    }
+    return last_var
 }
