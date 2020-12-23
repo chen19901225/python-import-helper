1560f373374838d315ffc4afc4a7769d9fc3d66d
test <test@qq.com>
2020-12-23 13:23:19 +0800

    insert_self

--- src/handler/handler_insert_self.ts
+++ src/handler/handler_insert_self.ts
@@ -1,13 +1,15 @@
 import * as vscode from "vscode";
 import { getConfig } from "../config";
+let start_text = "# cqh_insert_self:"
 let last_word: string = "";
-export function insert_self(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) { 
+
+export function insert_self(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
     let position = textEditor.selection.active;
     // edit.insert(position, "self");
 
     // let final_list = [];
     let quickPickItem: vscode.QuickPickItem[] = [];
-    if(last_word.length > 0) {
+    if (last_word.length > 0) {
         quickPickItem.push({
             "label": last_word,
             "description": last_word
@@ -16,13 +18,28 @@ export function insert_self(textEditor: vscode.TextEditor, edit: vscode.TextEdit
     let config = getConfig();
     let insert_list = config.insert_list;
     let index = 1;
-    for(let value of insert_list) {
+    for (let value of insert_list) {
         quickPickItem.push({
             "label": `${index}.${value}`,
             "description": value
         })
-        index = index +1
+        index = index + 1
     }
+    let document = textEditor.document;
+    const lines: string[] = document.getText().split(/\r?\n/g);
+    for (let line of lines) {
+        line = line.trim();
+        if (line.startsWith(start_text)) {
+            let word = line.slice(start_text.length)
+            word = word.trim();
+            quickPickItem.push({
+                "label": `${index}.${word}`,
+                "description": word
+            })
+            index = index + 1
+        }
+    }
+
     vscode.window.showQuickPick(quickPickItem).then(item => {
         if (item) {
             last_word = item.description;
